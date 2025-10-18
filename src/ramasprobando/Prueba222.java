c6982a98937346536e7fd2df96bc95497439bb91
gabrielaballerini-sketch <gabrielaballerini@gmail.com>
2025-10-18 19:37:47 -0300

    prue

--- src/ramasprobando/Prueba222.java
+++ src/ramasprobando/Prueba222.java
@@ -11,4 +11,45 @@ package ramasprobando;
  */
 public class Prueba222 {
     
+    private String prueba2;
+    
+    private String cdad;
+    private int sss;
+
+    public Prueba222(String prueba2, String cdad, int sss) {
+        this.prueba2 = prueba2;
+        this.cdad = cdad;
+        this.sss = sss;
+    }
+
+    public String getPrueba2() {
+        return prueba2;
+    }
+
+    public void setPrueba2(String prueba2) {
+        this.prueba2 = prueba2;
+    }
+
+    public String getCdad() {
+        return cdad;
+    }
+
+    public void setCdad(String cdad) {
+        this.cdad = cdad;
+    }
+
+    public int getSss() {
+        return sss;
+    }
+
+    public void setSss(int sss) {
+        this.sss = sss;
+    }
+
+    @Override
+    public String toString() {
+        return "Prueba222{" + "prueba2=" + prueba2 + ", cdad=" + cdad + ", sss=" + sss + '}';
+    }
+    
+    
 }
